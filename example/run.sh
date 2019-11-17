fd46b046432084d8c1a17ea7fc07af05c02916f2
Andrew Stanton <astanton@etsy.com>
2019-11-17 15:26:38 -0800

    Fixes

--- example/run.sh
+++ example/run.sh
@@ -4,6 +4,14 @@ python ../scripts/remap.py baseball.2018
 
 # Run the example with different model types
 pairwizer baseball.2018.remap glicko2 > baseball.2018.glicko2
-pairwizer baseball.2018.remap btm-mm > baseball.2018.btm
+python ../scripts/unmap.py baseball.2018.ids baseball.2018.glicko2 > scores.glicko2
+
+pairwizer baseball.2018.remap btm-mm > baseball.2018.btm-mm
+python ../scripts/unmap.py baseball.2018.ids baseball.2018.btm-mm > scores.btm-mm
+
+pairwizer baseball.2018.remap btm-lr > baseball.2018.btm-lr
+python ../scripts/unmap.py baseball.2018.ids baseball.2018.btm-lr > scores.btm-lr
+
 pairwizer baseball.2018.remap rate > baseball.2018.rate
+python ../scripts/unmap.py baseball.2018.ids baseball.2018.rate > scores.rate
 
