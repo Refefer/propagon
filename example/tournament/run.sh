2a4011b0cd292c0a6ee92f32980667c0a6098e94
Andrew Stanton <astanton@etsy.com>
2020-05-04 20:33:12 -0700

    Uses propagon's dehydrate/hydrate utility

--- example/tournament/run.sh
+++ example/tournament/run.sh
@@ -1,17 +1,17 @@
 # Computes the power rankings for baseball's 2018 season
 # This assumes you have the propagon binary compiled and installed!
-python ../scripts/remap.py baseball.2018
+propagon dehydrate baseball.2018
 
 # Run the example with different model types
-propagon baseball.2018.remap glicko2 > baseball.2018.glicko2
-python ../scripts/unmap.py baseball.2018.ids baseball.2018.glicko2 > scores.glicko2
+propagon baseball.2018.edges glicko2 > baseball.2018.glicko2
+propagon baseball.2018.glicko2 hydrate --vocab baseball.2018.vocab > scores.glicko2
 
-propagon baseball.2018.remap btm-mm > baseball.2018.btm-mm
-python ../scripts/unmap.py baseball.2018.ids baseball.2018.btm-mm > scores.btm-mm
+propagon baseball.2018.edges btm-mm > baseball.2018.btm-mm
+propagon baseball.2018.btm-mm hydrate --vocab baseball.2018.vocab > scores.btm-mm
 
-propagon baseball.2018.remap btm-lr > baseball.2018.btm-lr
-python ../scripts/unmap.py baseball.2018.ids baseball.2018.btm-lr > scores.btm-lr
+propagon baseball.2018.edges btm-lr > baseball.2018.btm-lr
+propagon baseball.2018.btm-lr hydrate --vocab baseball.2018.vocab > scores.btm-lr
 
-propagon baseball.2018.remap rate > baseball.2018.rate
-python ../scripts/unmap.py baseball.2018.ids baseball.2018.rate > scores.rate
+propagon baseball.2018.edges rate > baseball.2018.rate
+propagon baseball.2018.rate hydrate --vocab baseball.2018.vocab > scores.rate
 
